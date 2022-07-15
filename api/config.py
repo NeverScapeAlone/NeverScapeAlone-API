a559761dfee1c7c3acfb9a2322f89442a0a90f8a
Ferrariic <thearcticfalcon@gmail.com>
2022-07-15 16:13:17 -0500

    Increment version

--- api/config.py
+++ api/config.py
@@ -16,7 +16,7 @@ DISCORD_TOKEN = os.environ.get("discord_route_token")
 REDIS_PASSWORD = os.environ.get("redis_password")
 RATE_LIMIT_MINUTE = 120
 RATE_LIMIT_HOUR = 7200
-VERSION = "v1.3.0-alpha"
+VERSION = "v2.0.0-alpha"
 DEV_MODE = True
 
 redis_client = aioredis.from_url(
