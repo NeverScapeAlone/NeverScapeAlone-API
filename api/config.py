e7672a13ad59bd2e8476881ed58ef4b500fc270c
Ferrariic <thearcticfalcon@gmail.com>
2022-08-17 02:55:40 -0500

    Increments version

--- api/config.py
+++ api/config.py
@@ -19,8 +19,8 @@ DISCORD_WEBHOOK = os.environ.get("discord_webhook")
 REDIS_PASSWORD = os.environ.get("redis_password")
 RATE_LIMIT_MINUTE = 120
 RATE_LIMIT_HOUR = 7200
-VERSION = "v2.8.0-alpha"
-MATCH_VERSION = "v2.11.0-alpha"
+VERSION = "v2.8.1-alpha"
+MATCH_VERSION = "v2.11.1-alpha"
 DEV_MODE = False
 
 redis_client = aioredis.from_url(
