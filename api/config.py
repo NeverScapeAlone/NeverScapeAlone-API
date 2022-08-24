3e5f5c414ad300019dbe36701082d55e1d3ee764
Ferrariic <thearcticfalcon@gmail.com>
2022-08-23 22:26:48 -0500

    update versions

--- api/config.py
+++ api/config.py
@@ -23,8 +23,8 @@ class Configuration:
         self.REDIS_PASSWORD = os.environ.get("redis_password")
         self.RATE_LIMIT_MINUTE = 120
         self.RATE_LIMIT_HOUR = 7200
-        self.API_VERSION = "v2.12.0-alpha"
-        self.MATCH_VERSION = "v2.11.1-alpha"
+        self.API_VERSION = "v2.13.0-alpha"
+        self.MATCH_VERSION = "v2.14.0-alpha"
 
     def setMATCH_VERSION(self, match_version):
         self.MATCH_VERSION = match_version
