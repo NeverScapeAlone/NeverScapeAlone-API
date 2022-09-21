121e5af08e0287b1eb7fd51fca0238b799d50174
Ferrariic <thearcticfalcon@gmail.com>
2022-09-20 19:47:38 -0500

    try test again not async

--- tests/test_socket.py
+++ tests/test_socket.py
@@ -9,8 +9,7 @@ from api.app import app
 from fastapi.testclient import TestClient
 
 
-@pytest.mark.asyncio
-async def test_app():
+def test_app():
     client = TestClient(app=app)
     response = client.get("/")
     assert response.status_code == 200
